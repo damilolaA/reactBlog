ea20a2e37484006129d70e50d3f7d472b1fdef36
dammy <adiodamilola@yahoo.com>
2018-02-21 12:50:42 +0100

    define a Header component

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import Nav from './Nav';
+import Header from './Header';
 
 const Landing = () => (
   <div className="landing">
-    
     <Nav />
-
-    <div className="blog-header">
-      <div className="container">
-        <h1 className="blog-title">The Bootstrap Blog</h1>
-        <p className="lead blog-description">An example blog template built with Bootstrap.</p>
-      </div>
-    </div>
+    <Header />
   </div>
 );
 
--- js/Nav.jsx
+++ js/Nav.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav = () => (
-	<div className="blog-masthead">
-      <div className="container">
-        <nav className="nav blog-nav">
-          <Link className="nav-link active" to="#">
-            Home
-          </Link>
-          <Link className="nav-link" to="#">
-            New features
-          </Link>
-          <Link className="nav-link" to="#">
-            Press
-          </Link>
-          <Link className="nav-link" to="#">
-            New hires
-          </Link>
-          <Link className="nav-link" to="#">
-            About
-          </Link>
-        </nav>
-      </div>
+  <div className="blog-masthead">
+    <div className="container">
+      <nav className="nav blog-nav">
+        <Link className="nav-link active" to="#">
+          Home
+        </Link>
+        <Link className="nav-link" to="#">
+          New features
+        </Link>
+        <Link className="nav-link" to="#">
+          Press
+        </Link>
+        <Link className="nav-link" to="#">
+          New hires
+        </Link>
+        <Link className="nav-link" to="#">
+          About
+        </Link>
+      </nav>
     </div>
+  </div>
 );
 
 export default Nav;
