b56923705e218e074f1b2a9ebfbf842230c45cb3
dammy <adiodamilola@yahoo.com>
2018-02-22 10:59:29 +0100

    create footer component and include it to Landing component

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -3,18 +3,21 @@ import Nav from './Nav';
 import Header from './Header';
 import SideBar from './SideBar';
 import Posts from './Posts';
+import Footer from './Footer';
 
 const Landing = () => (
   <div className="landing">
     <Nav/>
     <Header/>
-    
     <div className="container">
     	<div className="row">
     		<Posts />
     		<SideBar/>
     	</div>
     </div>
+    
+    <Footer/>
+
   </div>
 );
 
