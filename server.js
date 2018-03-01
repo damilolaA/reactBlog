b6a855e4cb79bc9d2ffb5560de02bb68634472e6
dammy <adiodamilola@yahoo.com>
2018-03-01 01:22:10 +0100

    use express to serve static files

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+	  app	  = express(),
+	  port    = process.env.PORT || 2000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, (err) => {
+	if(err) {
+		return console.log(err)
+	}
+
+	console.log('server started at ' + port);
+})
